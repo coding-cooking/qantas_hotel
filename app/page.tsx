6564046b4e07958067daf1f887529d6f012d41eb
Coding-Cooking <fei.coding2023@gmail.com>
2025-03-16 21:18:04 +1100

    change to server fetching data and adjust readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
-"use client";
-
 import HotelPage from "@/components/HotelPage";
-import { useState } from "react";
 import { HotelType } from "@/types/hotel";
-import { useEffect } from "react";
 
-export default function Home() {
-  const [data, setData] = useState<HotelType[]>([]);
+async function getData(): Promise<HotelType[]> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/data`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return response.json();
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/data`);
-        const data = await response.json();
-        setData(data);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchData();
-  }, []);
-
+export default async function Home() {
+  const data = await getData();
   return <HotelPage data={data} />;
 }
